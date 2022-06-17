7298b7ec28f288d06aa1e6af26777d7db2b40e0d
Yulia Zherebtsova <juliaz1206@yandex.ru>
2022-06-17 07:43:15 +0300

    refactor: sorting algorithms render

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -3,53 +3,25 @@ import { Column } from 'components/ui/column/column';
 import { RadioInput } from 'components/ui/radio-input/radio-input';
 import { SolutionLayout } from 'components/ui/solution-layout/solution-layout';
 import { SHORT_DELAY_IN_MS } from 'constants/delays';
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Direction,
   ElementStates,
   SortAlgorithm,
   TDataElement,
 } from 'types/types';
-import { getRandomInt, swap, updateElementsWithInterval } from 'utils/utils';
+import { getRandomInt, updateElementsWithInterval } from 'utils/utils';
 import styles from './sorting-page.module.css';
 import { getSelectionSortSteps } from './utils';
 
 export const SortingPage: React.FC = () => {
-  const [arrayToSort, setArrayToSort] = useState<number[]>([]);
-  let [arrayOfElements, setArrayOfElements] = useState<TDataElement[]>([]);
+  const [arrayToSort, setArrayToSort] = useState<(TDataElement | null)[]>([]);
   const [isAscending, setIsAscending] = useState(false);
   const [inProgress, setInProgress] = useState(false);
   const [sortAlgorigthm, setSortAlgorithm] = useState<SortAlgorithm | null>(
     null,
   );
   const [isComponentMounted, setIsMounted] = useState(false);
-
-  const getRandomArray = () => {
-    const minLen = 3;
-    const maxLen = 17;
-    const minValue = 0;
-    const maxValue = 100;
-    const arrLength = getRandomInt(minLen, maxLen);
-    const randomArr = Array.from({ length: arrLength }, () =>
-      getRandomInt(minValue, maxValue),
-    );
-    arrayOfElements = [];
-    randomArr.map((num) => {
-      arrayOfElements!.push({
-        value: num,
-        state: ElementStates.Default,
-      });
-    });
-    console.log(randomArr, arrayOfElements);
-    setArrayToSort([...randomArr]);
-    setArrayOfElements(() => [...arrayOfElements]);
-  };
-
-  const selectionSort = async (isAscending: boolean) => {
-    const array = arrayToSort ? [...arrayToSort] : [];
-    const steps = getSelectionSortSteps(array, isAscending);
-    console.log(steps);
-  };
 
   useEffect(() => {
     setIsMounted(true);
@@ -60,63 +32,95 @@ export const SortingPage: React.FC = () => {
     };
   }, []);
 
-  // const bubbleSort = async (isAscending: boolean) => {
-  //   let arr = arrayToSort ? [...arrayToSort] : [];
-  //   if (isAscending) {
-  //     // по возрастанию
-  //     for (let i = 0; i < arr.length; i++) {
-  //       for (let j = 0; j < arr.length - i - 1; j++) {
-  //         arr[j]!.state = ElementStates.Changing;
-  //         arr[j + 1]!.state = ElementStates.Changing;
-  //         setArrayToSort([...arr]);
-  //         if (arr[j]!.value > arr[j + 1]!.value) {
-  //           swap(arr, j, j + 1);
-  //         }
-  //         await updateElementsWithInterval(
-  //           setArrayToSort,
-  //           arr,
-  //           SHORT_DELAY_IN_MS,
-  //           isComponentMounted,
-  //         );
-  //         arr[j]!.state = ElementStates.Default;
-  //         arr[j + 1]!.state = ElementStates.Default;
-  //       }
-  //       arr[arr.length - i - 1]!.state = ElementStates.Modified;
-  //       setArrayToSort([...arr]);
-  //     }
-  //   } else {
-  //     // по убыванию
-  //     for (let i = 0; i < arr.length; i++) {
-  //       for (let j = 0; j < arr.length - i - 1; j++) {
-  //         arr[j]!.state = ElementStates.Changing;
-  //         arr[j + 1]!.state = ElementStates.Changing;
-  //         setArrayToSort([...arr]);
-  //         if (arr[j]!.value < arr[j + 1]!.value) {
-  //           swap(arr, j, j + 1);
-  //         }
-  //         await updateElementsWithInterval(
-  //           setArrayToSort,
-  //           arr,
-  //           SHORT_DELAY_IN_MS,
-  //           isComponentMounted,
-  //         );
-  //         arr[j]!.state = ElementStates.Default;
-  //         arr[j + 1]!.state = ElementStates.Default;
-  //       }
-  //       arr[arr.length - i - 1]!.state = ElementStates.Modified;
-  //       setArrayToSort([...arr]);
-  //     }
-  //   }
-  // };
+  const getRandomArray = () => {
+    const minLen = 3;
+    const maxLen = 17;
+    const minValue = 0;
+    const maxValue = 100;
+    const arrLength = getRandomInt(minLen, maxLen);
+    const randomArray = Array.from({ length: arrLength }, () => ({
+      value: getRandomInt(minValue, maxValue),
+      state: ElementStates.Default,
+    }));
+    setArrayToSort([...randomArray]);
+  };
+
+  const selectionSort = async (isAscending: boolean) => {
+    const steps = getSelectionSortSteps(
+      arrayToSort ? [...arrayToSort] : [],
+      isAscending,
+    );
+    let currentStep = 0;
+    while (currentStep < steps.length) {
+      if (steps) {
+        await updateElementsWithInterval(
+          setArrayToSort,
+          [...steps[currentStep]],
+          SHORT_DELAY_IN_MS,
+          isComponentMounted,
+        );
+        currentStep++;
+      }
+    }
+  };
+
+  const bubbleSort = async (isAscending: boolean) => {
+    // let arr = arrayToSort ? [...arrayToSort] : [];
+    // if (isAscending) {
+    //   // по возрастанию
+    //   for (let i = 0; i < arr.length; i++) {
+    //     for (let j = 0; j < arr.length - i - 1; j++) {
+    //       arr[j]!.state = ElementStates.Changing;
+    //       arr[j + 1]!.state = ElementStates.Changing;
+    //       setArrayToSort([...arr]);
+    //       if (arr[j]!.value > arr[j + 1]!.value) {
+    //         swap(arr, j, j + 1);
+    //       }
+    //       await updateElementsWithInterval(
+    //         setArrayToSort,
+    //         arr,
+    //         SHORT_DELAY_IN_MS,
+    //         isComponentMounted,
+    //       );
+    //       arr[j]!.state = ElementStates.Default;
+    //       arr[j + 1]!.state = ElementStates.Default;
+    //     }
+    //     arr[arr.length - i - 1]!.state = ElementStates.Modified;
+    //     setArrayToSort([...arr]);
+    //   }
+    // } else {
+    //   // по убыванию
+    //   for (let i = 0; i < arr.length; i++) {
+    //     for (let j = 0; j < arr.length - i - 1; j++) {
+    //       arr[j]!.state = ElementStates.Changing;
+    //       arr[j + 1]!.state = ElementStates.Changing;
+    //       setArrayToSort([...arr]);
+    //       if (arr[j]!.value < arr[j + 1]!.value) {
+    //         swap(arr, j, j + 1);
+    //       }
+    //       await updateElementsWithInterval(
+    //         setArrayToSort,
+    //         arr,
+    //         SHORT_DELAY_IN_MS,
+    //         isComponentMounted,
+    //       );
+    //       arr[j]!.state = ElementStates.Default;
+    //       arr[j + 1]!.state = ElementStates.Default;
+    //     }
+    //     arr[arr.length - i - 1]!.state = ElementStates.Modified;
+    //     setArrayToSort([...arr]);
+    //   }
+    // }
+  };
 
   const sortArray = async (isAscending: boolean) => {
     setInProgress(true);
     if (sortAlgorigthm === SortAlgorithm.selectsort) {
       await selectionSort(isAscending);
     }
-    // if (sortAlgorigthm === SortAlgorithm.bubble) {
-    //   await bubbleSort(isAscending);
-    // }
+    if (sortAlgorigthm === SortAlgorithm.bubble) {
+      await bubbleSort(isAscending);
+    }
     setInProgress(false);
   };
 
@@ -174,7 +178,7 @@ export const SortingPage: React.FC = () => {
         </div>
       </div>
       <div className={styles.barsContainer}>
-        {arrayOfElements?.map((element, index) => (
+        {arrayToSort?.map((element, index) => (
           <Column
             index={parseInt(element!.value.toString())}
             state={element?.state}
--- src/components/sorting-page/utils.ts
+++ src/components/sorting-page/utils.ts
@@ -1,35 +1,86 @@
+import { ElementStates, TDataElement } from 'types/types';
 import { swap } from 'utils/utils';
 
 export const getSelectionSortSteps = (
-  arr: number[],
+  tmp: (TDataElement | null)[],
   isAscending: boolean,
-): number[][] => {
-  const steps: number[][] = [];
+) => {
+  const arr = tmp ? [...tmp] : [];
+  const steps: (TDataElement | null)[][] = [];
   if (isAscending) {
     // по возрастанию
     for (let i = 0; i < arr.length - 1; i++) {
       let indToSwap = i;
+      arr[i] = {
+        ...arr[i],
+        state: ElementStates.Changing,
+      } as TDataElement;
       for (let j = i + 1; j < arr.length; j++) {
-        if (arr[j] < arr[indToSwap]) {
+        arr[j] = {
+          ...arr[j],
+          state: ElementStates.Changing,
+        } as TDataElement;
+        steps.push([...arr]);
+        if (arr[j]!.value < arr[indToSwap]!.value) {
           indToSwap = j;
         }
+        arr[j] = {
+          ...arr[j],
+          state: ElementStates.Default,
+        } as TDataElement;
       }
+      arr[i] = {
+        ...arr[i],
+        state: ElementStates.Default,
+      } as TDataElement;
       swap(arr, i, indToSwap);
-      steps.push([...arr]);
+      arr[i] = {
+        ...arr[i],
+        state: ElementStates.Modified,
+      } as TDataElement;
     }
+    arr[arr.length - 1] = {
+      ...arr[arr.length - 1],
+      state: ElementStates.Modified,
+    } as TDataElement;
+    steps.push([...arr]);
   } else {
     // по убыванию
     for (let i = 0; i < arr.length - 1; i++) {
       let indToSwap = i;
+      arr[i] = {
+        ...arr[i],
+        state: ElementStates.Changing,
+      } as TDataElement;
       for (let j = i + 1; j < arr.length; j++) {
-        if (arr[j] > arr[indToSwap]) {
+        arr[j] = {
+          ...arr[j],
+          state: ElementStates.Changing,
+        } as TDataElement;
+        steps.push([...arr]);
+        if (arr[j]!.value > arr[indToSwap]!.value) {
           indToSwap = j;
         }
+        arr[j] = {
+          ...arr[j],
+          state: ElementStates.Default,
+        } as TDataElement;
       }
+      arr[i] = {
+        ...arr[i],
+        state: ElementStates.Default,
+      } as TDataElement;
       swap(arr, i, indToSwap);
-      steps.push(arr);
+      arr[i] = {
+        ...arr[i],
+        state: ElementStates.Modified,
+      } as TDataElement;
     }
+    arr[arr.length - 1] = {
+      ...arr[arr.length - 1],
+      state: ElementStates.Modified,
+    } as TDataElement;
+    steps.push([...arr]);
   }
-  console.log(steps);
   return steps;
 };
